7cd2f4c6c7e5cee91be93328a118cfe11c4173e4
xxxDee <karl.ryandee1511@gmail.com>
2024-10-02 13:11:42 +0800

    initial commit

---
+++ src/components/canvas/Stars.jsx
@@ -0,0 +1,54 @@
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+
+const Stars = (props) => {
+  const ref = useRef();
+  const [sphere, setSphere] = useState(() => {
+    const arr = new Float32Array(3 * 5000);
+    for (let i = 0; i < 5000; i++) {
+      const x = Math.random() * 2 - 1;
+      const y = Math.random() * 2 - 1;
+      const z = Math.random() * 2 - 1;
+      arr[i * 3] = x;
+      arr[i * 3 + 1] = y;
+      arr[i * 3 + 2] = z;
+    }
+    return arr;
+  });
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color='#f272c8'
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarsCanvas = () => {
+  return (
+    <div className='w-full h-auto absolute inset-0 z-[-1]'>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
---
+++ src/constants/index.js
@@ -0,0 +1,216 @@
+import {
+    mobile,
+    backend,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    logo,
+    vape,
+    jnt,
+    heineken,
+    iapro,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "VapeApe. Sales Assist",
+      company_name: "VapeApe",
+      icon: vape,
+      iconBg: "#383E56",
+      date: "November 2023 - August 2024",
+      points: [
+        "Assisting customers and ensuring smooth transactions in a retail setting.",
+        "Answering questions about products or services.",
+        "Processing sales of merchandise in-store or online.",
+        "Supervising restocking of product shelves",
+      ],
+    },
+    {
+      title: "Admin Clerk. J&T",
+      company_name: "J&t",
+      icon: jnt,
+      iconBg: "#E6DEDD",
+      date: "November 2023 - August 2024",
+      points: [
+        "Contacting clients via phone or email",
+        "Organizing and storing files.",
+        "Managing office supplies.",
+        "Making copies of documents.",
+      ],
+    },
+    {
+      title: "D'Tiki Bartender",
+      company_name: "D'Tiki Shack",
+      icon: heineken,
+      iconBg: "#383E56",
+      date: "February 2018 - Jun 2022",
+      points: [
+        "Mix and serve alcoholic beverages based on customer requests.",
+        "Verify the identification and age of customers.",
+        "Accept payment from customers.",
+        "Clean glasses and bar utensils.",
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "IA",
+      icon: logo,
+      iconBg: "#E6DEDD",
+      date: "September 2024 - Until Now",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Karl proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Karl does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Karl optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "IA | Portfolio",
+      description:
+        "Web-based platform that allows users to see my portfolio. Starting from who am i, what i do, my experience and my Project.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Javascript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: iapro,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
